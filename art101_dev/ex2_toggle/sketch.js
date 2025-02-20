bfc010909c1a1ce489a38e92e2d65727d3f18e54
ellasatorre <ella.satorre@gmail.com>
2025-02-20 14:26:09 -0800

    publish template

---
+++ art101_dev/ex2_toggle/sketch.js
@@ -0,0 +1,42 @@
+let choice;
+let cvalue;
+
+
+function setup() {
+  createCanvas(500,500);
+  background(100);
+  
+  choice = "left";
+  
+  cvalue = 0;
+
+
+}
+
+function draw() {
+
+fill(cvalue);
+ ellipse(width/2, height/2, 50, 50);
+}
+
+function keyPressed(){
+
+  if (key == 'b' || key == 'B'){ 
+  if (cvalue == 0) {
+    cvalue = 255;
+
+  } else {
+    cvalue = 0;
+
+  } 
+  }
+
+if ( key == 'c' || key == 'C'){
+  if (choice == 'left'){
+    cvalue = 128;
+  }
+}
+
+}
+
+
