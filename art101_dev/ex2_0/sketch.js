b726c14f873445bd69482b7bcdc1a54ed246042e
ellasatorre <ella.satorre@gmail.com>
2025-02-25 23:09:00 -0800

    proj 1

--- art101_dev/ex2_0/sketch.js
+++ art101_dev/ex2_0/sketch.js
@@ -3,9 +3,13 @@ let seal1;
 let seal2;
 let bgseal;
 let t = 0;
+let counter = 0;
+let counter2 = 0;
+let row2 = 300;
+
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(1000,1000);
   seal1 = loadImage("assets/sealhat.png");
   seal2 = loadImage("assets/whiteseal.png");
   bgseal = loadImage("assets/happyseal.jpg");
@@ -41,5 +45,21 @@ function draw() {
       image(seal2, i, g, 100, 100);
      }
     
+  }
+
+  image(seal1, counter, 0, 100, 100);
+  image(seal1, 0, counter, 100, 100);
+
+  image(seal1, counter + row2, 0 + row2, 100, 100);
+  image(seal1, 0 + row2, counter + row2, 100, 100);
+
+  image(seal1, counter2 + 300, 0 + 300, 100, 100);
+  image(seal1, 0 + 300, counter2 + 300, 100, 100);
+
+
+  if (counter2 > height) {
+    counter2 = 0;
+  } else {
+    counter2 += 100;
   }
 }
