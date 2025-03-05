e328df1b192442e2969598a2cbfbdd467eb11542
ellasatorre <ella.satorre@gmail.com>
2025-03-04 18:11:12 -0800

    made bunny

---
+++ art101_dev/ex3_0/sketch.js
@@ -0,0 +1,88 @@
+let bgc;
+let cloud;
+
+function preload(){
+  cloud = loadImage("assets/cloud.png");
+}
+
+function setup() {
+  createCanvas(750,750);
+  colorMode(HSB);
+  bgc = color(random(170,220), 60, 70);
+  colorMode(RGB);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+  imageMode(CENTER);
+  noStroke();
+}
+
+function draw() {
+  background(bgc);
+
+  let c = color(115, 107, 100);
+  let angle = frameCount/10;
+
+
+  bunnyHead(c, width/2, height/2,angle,1.5);
+  clouds(100, 100);
+}
+
+function bunnyHead(c,lyo,lxo,rot,sc){
+  push();
+translate(lyo, lxo);
+rotate(rot);
+scale(sc);
+fill(c);
+
+  bunnyEars(c, -60, -65, -10);
+  bunnyEars(c, 60, -65, 10);
+  ellipse(0,0,190,165);
+
+  bunnyEyes(-50, 10);
+  bunnyEyes(50, 10);
+
+  bunnyNose(0, 15);
+  pop();
+}
+
+function bunnyEars(c,lyo,lxo,rot,sc){
+  push();
+translate(lyo, lxo);
+rotate(rot);
+scale(sc);
+fill(c);
+  rect(0, 0,  50, 200, 45, 45, 15, 15);
+  fill(252, 162, 207);
+  rect(0, 5,  30, 170, 45, 45, 15, 15);
+  pop();
+}
+
+function bunnyEyes(lyo,lxo,rot,sc){
+  push();
+translate(lyo, lxo);
+rotate(rot);
+scale(sc);
+  fill(0);
+  ellipse(0, 0, 20, 30);
+  pop();
+}
+
+function bunnyNose(lyo,lxo,rot,sc){
+  push();
+translate(lyo, lxo);
+rotate(rot);
+scale(sc);
+  fill(252, 162, 207);
+  ellipse(0, 0, 15, 10);
+pop();
+}
+
+function clouds(lxo,lyo,rot,sc){
+push();
+translate(lxo,lyo)
+rotate(rot);
+scale(sc);
+image(cloud, 0, 0, 100, 50);
+
+pop();
+}
